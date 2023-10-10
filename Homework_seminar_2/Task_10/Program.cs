4a0e40be31cb87879b7b3b08f3a5283cc6b7c778
NiKiTa <hukuta199701@gmail.com>
2023-10-10 21:26:08 +0300

    HW_S2_T10 sucсess

---
+++ Homework_seminar_2/Task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int ReadInt(string msg)
+{
+    System.Console.WriteLine(msg);
+    string userNumberForParse = Console.ReadLine();
+    int userNumber;
+    userNumber = int.Parse(userNumberForParse);
+    return userNumber;
+}
+int userNumber = ReadInt("Введите число:");
+int secondNumber = userNumber /10 % 10;
+System.Console.WriteLine(secondNumber);
