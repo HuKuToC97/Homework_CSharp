767b6f1c9154797ae671c8b4e2588a64277e4ee8
NiKiTa <hukuta199701@gmail.com>
2023-10-11 21:38:35 +0300

    HW_s2_t15 success

--- Homework_seminar_2/Task_15/Program.cs
+++ Homework_seminar_2/Task_15/Program.cs
@@ -4,3 +4,23 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+System.Console.WriteLine("Введите цифру от 1 до 7, обозначающую день недели ");
+// int userNumber = int.Parse(Console.ReadLine());
+string userNumber = Console.ReadLine();
+switch (userNumber)
+{
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+        System.Console.WriteLine("Этот будний день");
+        break;
+    case "6":
+    case "7":
+        System.Console.WriteLine("Этот день выходной");
+        break;
+    default:
+        System.Console.WriteLine("Нет подходящего дня недели, попробуйте перезапустить программу и ввести цифру от 1 до 7");
+        break;
+}
