30578a35879c9dae6b2c1f31c1c3f9b91bc96012
NiKiTa <hukuta199701@gmail.com>
2023-10-11 21:21:10 +0300

    HW_s2_t13 success

--- Homework_seminar_2/Task_13/Program.cs
+++ Homework_seminar_2/Task_13/Program.cs
@@ -7,41 +7,75 @@
 
 int ReadInt(string msg)
 {
-    System.Console.WriteLine(msg);
+    System.Console.Write(msg);
     string userNumberForParse = Console.ReadLine();
     int userNumber;
     userNumber = int.Parse(userNumberForParse);
     return userNumber;
 }
 
-int ReversalNumber(int normalNumber)
-{
-    int reversalNumber = 0;
-    while (normalNumber > 0)
-    {
-        reversalNumber *= 10;
-        reversalNumber += normalNumber % 10;
-        normalNumber /= 10;
-    }
-    System.Console.WriteLine(reversalNumber);
-    return reversalNumber;
-}
 
 int userNumber = ReadInt("Введите число:");
-int reversalNumber = ReversalNumber(userNumber);
-
-if (userNumber > 99) /// НАДО ДЕЛАТЬ ЧЕРЕЗ МАССИВ
+int countNumber = userNumber;
+int i = 0;
+while (countNumber > 9)
 {
-    if (userNumber % 10 == 0)
-    {
-        System.Console.WriteLine();
-    }
-    else
-    {
-        System.Console.WriteLine(reversalNumber / 100 % 10);
-    }
+    countNumber /= 10;
+    i++;
+    // System.Console.WriteLine($"Проход №{i} число {countNumber}");
+}
+
+if (i <= 1)
+{
+    System.Console.WriteLine($"{userNumber} -> третьей цифры нет");
 }
 else
 {
-    System.Console.WriteLine("третьей цифры нет");
+    int helpCount = (int)Math.Pow(10, (i - 2));
+    System.Console.WriteLine($"{userNumber} -> {userNumber / helpCount % 10}");
 }
+
+
+
+// Ниже остатки кода, решил их сохранить для себя.  
+//В начале планировал через разворот числа, потом подумал,/
+// что можно через массив, в итоге пришел к решению выше
+// int ReversalNumber(int normalNumber)
+// {
+//     int reversalNumber = 0;
+//     while (normalNumber > 0)
+//     {
+//         reversalNumber *= 10;
+//         reversalNumber += normalNumber % 10;
+//         normalNumber /= 10;
+//     }
+//     System.Console.WriteLine(reversalNumber);
+//     return reversalNumber;
+// }
+
+// int reversalNumber = ReversalNumber(userNumber);
+
+
+
+// if (userNumber > 99) 
+// {
+//     if (userNumber % 10 == 0)
+//     {
+//         System.Console.WriteLine();
+//     }
+//     else
+//     {
+//         System.Console.WriteLine(reversalNumber / 100 % 10);
+//     }
+// }
+// else
+// {
+//     System.Console.WriteLine("третьей цифры нет");
+// }
+
+
+
+
+
+
+
