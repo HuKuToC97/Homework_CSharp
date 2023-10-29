8cb359294b7535bf7c0c4010db404f553af564b6
NiKiTa <hukuta199701@gmail.com>
2023-10-29 08:26:39 +0300

    HW_s6 in procces t1_success

--- Homework_seminar_6/Task_41/Program.cs
+++ Homework_seminar_6/Task_41/Program.cs
@@ -1,58 +1,93 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}   ");
+    }
+    System.Console.WriteLine();
+}
+
 void EndProgram()
 {
-System.Console.WriteLine("Для выхода из программы нажмите любую клавишу");
-System.Console.ReadKey();
-System.Console.Clear();
+    System.Console.WriteLine("Для выхода из программы нажмите любую клавишу");
+    System.Console.ReadKey();
+    System.Console.Clear();
 }
 
 
 string SaveNumbersReceivedFromTheUser(string msg)
 {
     System.Console.Clear();
-    string userNumbers = " ";
+    string userNumbers = "|";
+    string bufferString = "";
+    bool stopCheck = true;
     char stopReading = '#';
-    string heading = $"Для того чтобы остановить ввод чисел ввидите символ {stopReading}";
+    string heading = $"Для того чтобы остановить ввод чисел ввидите символ {stopReading}\nЧтобы ввести следующее число нажмите клавишу Enter";
     System.Console.WriteLine(heading);
     System.Console.Write(msg);
-    while (userNumbers[userNumbers.Length - 1] != stopReading)
+    // while (userNumbers[userNumbers.Length-1] != stopReading)
+    while (stopCheck != userNumbers.EndsWith("#|"))
     {
-        userNumbers = userNumbers + System.Console.ReadLine();
+        bufferString = System.Console.ReadLine();
+        userNumbers += bufferString + "|";
         System.Console.Clear();
         System.Console.WriteLine(heading);
         System.Console.Write(msg);
         for (int i = 0; i < userNumbers.Length; i++)
         {
-            System.Console.Write(userNumbers[i]+"|");
+            System.Console.Write(userNumbers[i]);
         }
 
     }
     System.Console.Clear();
-    return userNumbers.Remove(userNumbers.Length - 1);
+    return userNumbers.Remove(userNumbers.Length - 2);
 }
 
-string userNumbers = SaveNumbersReceivedFromTheUser("Введите ваши числа: ");
-System.Console.WriteLine($"Ваш массив:{userNumbers}");
+int[] ConvertUserStrigToArrayNumbers(string userStringNumbers)
+{
+    int[] userArrayNumbers = { 0 };
+    int countRecords = 0;
+    string dataBuffer = "";
+    for (int i = 0; i < userStringNumbers.Length; i++)
+    {
+        if (userStringNumbers[i] != '|')
+        {
+            while (userStringNumbers[i] != '|')
+            {
+                dataBuffer += userStringNumbers[i];
+                i++;
+            }
+            userArrayNumbers[countRecords] = Convert.ToInt32(dataBuffer);
+            Array.Resize(ref userArrayNumbers, userArrayNumbers.Length + 1);
+            dataBuffer = "";
+            countRecords++;
+        }
+    }
+    Array.Resize(ref userArrayNumbers, userArrayNumbers.Length - 1);
+    return userArrayNumbers;
+}
+
+int NumberOfPositiveNumbersInTheArray(int[] userArray)
+{
+    int countPositiveNumbers = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i] > 0)
+        {
+            countPositiveNumbers++;
+        }
+    }
+    return countPositiveNumbers;
+}
+
+string userNumbersInString = SaveNumbersReceivedFromTheUser("Введите ваши числа: ");
+System.Console.WriteLine($"Ваш массив:{userNumbersInString}");
+System.Console.WriteLine();
+int[] userArray = ConvertUserStrigToArrayNumbers(userNumbersInString);
+PrintArray(userArray);
+System.Console.WriteLine($"Количество позитивных чисел в массиве равно: {NumberOfPositiveNumbersInTheArray(userArray)}");
 
 EndProgram();
-
-// void PrintUserNubers(string userNumbers)
-// {
-
-//     for (int i = 0; i < userNumbers.Length; i++)
-//     {
-//         if (userNumbers[i] != ',')
-//         {
-//             System.Console.Write(userNumbers[i]);
-//         }
-//         else
-//         {
-//             System.Console.Write(" ");
-//         }
-//     }
-
-// }
-// string userNumbers = "1,4,-5,15,25";
-// PrintUserNubers(userNumbers);
